fce1b570ac82385a42b18b8674258e4ea4f29125
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-09-10 21:39:53 +0900

    Update I2C interrupt header

--- src/i2c/c/HAL/Interrupt/interrupt.h
+++ src/i2c/c/HAL/Interrupt/interrupt.h
@@ -8,13 +8,12 @@
 #include "../../Driver/FND/fnd.h"
 #include "../../Driver/I2C/i2c_ctrl.h"
 
-#define INTC_DEV_ID 	XPAR_INTC_0_DEVICE_ID
-#define TIMER0_INTR_ID 	XPAR_INTC_0_TMR_0_VEC_ID
-#define I2C_INTR_ID		XPAR_INTC_0_AXI_I2C_0_VEC_ID
+#define INTC_DEV_ID      XPAR_INTC_0_DEVICE_ID
+#define TIMER0_INTR_ID   XPAR_INTC_0_TMR_0_VEC_ID
+#define I2C_INTR_ID      XPAR_INTC_0_AXI_I2C_0_VEC_ID
 
 void TMR0_ISR(void *CallbackRef);
 void I2C_ISR(void *CallbackRef);
-int SetupInterruptSystem();
+int SetupInterruptSystem(void);
 
-
-#endif /* SRC_COMMON_INTERRUPT_H_ */
+#endif /* SRC_HAL_INTERRUPT_H_ */
