484ee926a8b32cc31bc00b2b6bdee8e90283ddfb
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-09-10 21:53:21 +0900

    Clean up I2C application source comments

--- src/i2c/c/ap/ap_main.c
+++ src/i2c/c/ap/ap_main.c
@@ -15,47 +15,45 @@ hBtn_t hbtnRead;
 uint8_t swData = 0;
 uint8_t i2cReadData = 0;
 
-void ap_init()
+void ap_init(void)
 {
-	Button_Init(&hbtnWrite,    GPIOA, GPIO_PIN_4);
-	Button_Init(&hbtnRead,     GPIOA, GPIO_PIN_5);
+    Button_Init(&hbtnWrite, GPIOA, GPIO_PIN_4);
+    Button_Init(&hbtnRead, GPIOA, GPIO_PIN_5);
 
-	Switch_Init();
-	FND_Init();
-	I2C_Init(I2C);
-	I2C_CtrlInit();
-	SetupInterruptSystem();
+    Switch_Init();
+    FND_Init();
+    I2C_Init(I2C);
+    I2C_CtrlInit();
+    SetupInterruptSystem();
 
-	//I2C interrupt
-	I2C_SetIntr(I2C, 1);
+    // Enable I2C interrupt
+    I2C_SetIntr(I2C, 1);
 
-	// 1ms interrupt
-	TMR_SetPSC(TMR0, 100-1);
-	TMR_SetARR(TMR0, 1000-1);
-	TMR_StartIntr(TMR0);
-	TMR_StartTimer(TMR0);
-
-
+    // Configure 1 ms timer interrupt
+    TMR_SetPSC(TMR0, 100 - 1);
+    TMR_SetARR(TMR0, 1000 - 1);
+    TMR_StartIntr(TMR0);
+    TMR_StartTimer(TMR0);
 }
 
-void ap_execute()
+void ap_execute(void)
 {
     // I2C Write
-    if(Button_GetState(&hbtnWrite) == ACT_RELEASED){
+    if (Button_GetState(&hbtnWrite) == ACT_RELEASED) {
         swData = Switch_Read();
         I2C_SendData(0x12, swData);
     }
 
     // I2C Read
-    if(Button_GetState(&hbtnRead) == ACT_RELEASED){
+    if (Button_GetState(&hbtnRead) == ACT_RELEASED) {
         I2C_ReadData(0x12);
     }
 
-    // I2C Read ¿Ï·á
-    if(I2C_IsReadDone()){
+    // Read completed data
+    if (I2C_IsReadDone()) {
         i2cReadData = I2C_GetReadData();
     }
 
-    // FND Display
+    // Display received data on FND
     FND_SetNum(i2cReadData);
 }
