911e34fef68e42046cd03d63d45db248ff30514b
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-09-10 21:55:07 +0900

    Clean up I2C HAL header

--- src/i2c/c/HAL/I2C/i2c.h
+++ src/i2c/c/HAL/I2C/i2c.h
@@ -6,29 +6,27 @@
 
 #define I2C_BASE_ADDR XPAR_AXI_I2C_0_S00_AXI_BASEADDR
 
+#define I2C_CR_CLK_DIV_SHIFT 8
+#define I2C_CR_CLK_DIV_MASK  (0xFFFFU << I2C_CR_CLK_DIV_SHIFT)
 
+#define I2C_CR_START   (1U << 0)
+#define I2C_CR_WRITE   (1U << 1)
+#define I2C_CR_READ    (1U << 2)
+#define I2C_CR_STOP    (1U << 3)
+#define I2C_CR_ACK_IN  (1U << 4)
+#define I2C_CR_INTR_EN (1U << 5)
 
-#define I2C_CR_CLK_DIV_SHIFT 	8
-#define I2C_CR_CLK_DIV_MASK     (0xFFFFU << I2C_CR_CLK_DIV_SHIFT)
-
-#define I2C_CR_START    	(1U << 0)
-#define I2C_CR_WRITE    	(1U << 1)
-#define I2C_CR_READ    		(1U << 2)
-#define I2C_CR_STOP   		(1U << 3)
-#define I2C_CR_ACK_IN    	(1U << 4)
-#define I2C_CR_INTR_EN    	(1U << 5)
-
-#define I2C_SR_BUSY    		(1U << 0)
-#define I2C_SR_ACK_OUT    	(1U << 1)
+#define I2C_SR_BUSY    (1U << 0)
+#define I2C_SR_ACK_OUT (1U << 1)
 
 typedef struct {
-	volatile uint32_t SR;
-	volatile uint32_t TXDR;
-	volatile uint32_t RXDR;
-	volatile uint32_t CR;
+    volatile uint32_t SR;
+    volatile uint32_t TXDR;
+    volatile uint32_t RXDR;
+    volatile uint32_t CR;
 } I2C_Typedef_t;
 
-#define I2C ((I2C_Typedef_t *) (I2C_BASE_ADDR))
+#define I2C ((I2C_Typedef_t *)I2C_BASE_ADDR)
 
 void I2C_Init(I2C_Typedef_t *I2Cx);
 void I2C_Start(I2C_Typedef_t *I2Cx);
@@ -41,4 +39,4 @@ void I2C_SetAck(I2C_Typedef_t *I2Cx, uint8_t ack);
 void I2C_SetIntr(I2C_Typedef_t *I2Cx, uint8_t enable);
 void I2C_SetClkDiv(I2C_Typedef_t *I2Cx, uint16_t clk_div);
 
-#endif /* SRC_DRIVER_I2C_I2C_H_ */
+#endif /* SRC_HAL_I2C_H_ */
