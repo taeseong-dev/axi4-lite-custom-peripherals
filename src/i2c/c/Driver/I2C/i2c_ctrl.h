80870914dd82d25d2d71deec89417a3d0f2dec23
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-09-10 21:55:23 +0900

    Clean up I2C driver header

--- src/i2c/c/Driver/I2C/i2c_ctrl.h
+++ src/i2c/c/Driver/I2C/i2c_ctrl.h
@@ -7,10 +7,8 @@
 void I2C_CtrlInit(void);
 void I2C_SendData(uint8_t slave_addr, uint8_t data);
 void I2C_ReadData(uint8_t slave_addr);
-
 uint8_t I2C_GetReadData(void);
 uint8_t I2C_IsReadDone(void);
-
 void I2C_Handler(void);
 
-#endif /* SRC_DRIVER_I2C_I2C_H_ */
+#endif /* SRC_DRIVER_I2C_CTRL_H_ */
