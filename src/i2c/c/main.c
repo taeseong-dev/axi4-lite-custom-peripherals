9ddacf50ad6005594e87d191b6e1a7c9af29c746
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-09-10 21:53:04 +0900

    Clean up I2C main source comments

--- src/i2c/c/main.c
+++ src/i2c/c/main.c
@@ -1,16 +1,15 @@
 #include "ap/ap_main.h"
 #include "xparameters.h"
 
-// main 함수 : system 초기화 및 실행
-int main() {
+// System initialization and main execution loop
+int main(void)
+{
+    ap_init();
 
-	ap_init();
+    while (1)
+    {
+        ap_execute();
+    }
 
-	while(1)
-	{
-		ap_execute();
-	}
-	return 0;
-
+    return 0;
 }
-
